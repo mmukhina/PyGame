27b12ee528c04adb152b3164d6d422e12bc2cd57
Mayya <mayya.mukhina@icloud.com>
2021-12-24 15:03:01 +0300

    player rotation

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ floor_rect.center = floor_cordx, floor_cordy
 
 player_rect = char.get_rect()
 player_rect.center = player_cordx, player_cordy
+rotation = 0
 
 while running:
     for event in pygame.event.get():
@@ -36,6 +37,9 @@ while running:
         elif player_cordy <= 350:
             player_cordy += 1
 
+        char = pygame.transform.rotate(char, abs(360 - rotation + 180))
+        rotation = 180
+
     elif keys[K_UP] or keys[K_w]:
         if floor_cordy >= 3500 and player_cordy >= 30 and player_cordy <= 350:
             player_cordy -= 1
@@ -43,6 +47,9 @@ while running:
             floor_cordy += 1
         elif player_cordy >= 350:
             player_cordy -= 1
+
+        char = pygame.transform.rotate(char, abs(360 - rotation))
+        rotation = 0
       
     elif keys[K_LEFT] or keys[K_a]:
         if floor_cordx >= 4500 and player_cordx >= 30 and player_cordx <= 450:
@@ -51,6 +58,9 @@ while running:
             floor_cordx += 1
         elif player_cordx >= 450:
             player_cordx -= 1
+
+        char = pygame.transform.rotate(char, abs(360 - rotation + 90))
+        rotation = 90
             
     elif keys[K_RIGHT] or keys[K_d]:
         if floor_cordx <= -3600 and player_cordx <= 870 and player_cordx >= 450:
@@ -60,12 +70,14 @@ while running:
         elif player_cordx <= 450:
             player_cordx += 1
 
+        char = pygame.transform.rotate(char, abs(360 - rotation + 270))
+        rotation = 270
+
+
+
     floor_rect.center = floor_cordx, floor_cordy
     player_rect.center = player_cordx, player_cordy
 
-    
-    
-    screen.fill(GRAY)
     screen.blit(floor, floor_rect)
     screen.blit(char, player_rect)
     pygame.display.update()
