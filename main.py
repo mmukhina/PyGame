1a0eb8231d724e7b48f085ca709fc3d5535f14c8
Mayya <mayya.mukhina@icloud.com>
2021-12-28 10:46:55 +0300

    начало игрыЭ

--- main.py
+++ main.py
@@ -1,20 +1,52 @@
 import pygame
 from pygame.locals import *
+import pyautogui
 
-RED = (255, 0, 0)
-GRAY = (150, 150, 150)
+def open_start():
+    w, h = 900, 700
+    pygame.init()
+    screen_start = pygame.display.set_mode((w, h))
+    running = 1
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+                
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                running = 0
 
+    pygame.quit()
+
+def open_stop():
+    w, h = 900, 700
+    pygame.init()
+    screen_start = pygame.display.set_mode((w, h))
+    running = 1
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+                
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                running = 0
+
+    pygame.quit
+
+
+open_start()
+
+
+width, height = pyautogui.size()
 pygame.init()
-w, h = 900, 700
-screen = pygame.display.set_mode((w, h))
+screen = pygame.display.set_mode((width, height))
 running = True
 
 floor = pygame.image.load('pictures/map.png')
 char = pygame.image.load("pictures/char.png")
-player_cordx = 900 // 2 
-player_cordy = 700 // 2
-floor_cordx = 900 // 2
-floor_cordy = 700 // 2
+player_cordx = width // 2 
+player_cordy = height // 2
+floor_cordx = width // 2
+floor_cordy = height // 2
 
 floor_rect = floor.get_rect()
 floor_rect.center = floor_cordx, floor_cordy
@@ -22,62 +54,81 @@ floor_rect.center = floor_cordx, floor_cordy
 player_rect = char.get_rect()
 player_rect.center = player_cordx, player_cordy
 rotation = 0
+speed = 5
+count = 0
 
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
 
+        if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    open_start()
+
     keys = pygame.key.get_pressed()
+
     if keys[K_DOWN] or keys[K_s]:
-        if floor_cordy <= -2800 and player_cordy <= 670 and player_cordy >= 350:
-            player_cordy += 1
-        elif floor_cordy >= -2800 and player_cordy == 350:
-            floor_cordy -= 1
-        elif player_cordy <= 350:
-            player_cordy += 1
+        if floor_cordy <= -2420 and player_cordy <= 1050 and player_cordy >= height // 2:
+            player_cordy += speed
+        elif floor_cordy >= -2420 and player_cordy == height // 2:
+            floor_cordy -= speed
+        elif player_cordy <= height // 2:
+            player_cordy += speed
 
         char = pygame.transform.rotate(char, abs(360 - rotation + 180))
         rotation = 180
 
     elif keys[K_UP] or keys[K_w]:
-        if floor_cordy >= 3500 and player_cordy >= 30 and player_cordy <= 350:
-            player_cordy -= 1
-        elif floor_cordy <= 3500 and player_cordy == 350:
-            floor_cordy += 1
-        elif player_cordy >= 350:
-            player_cordy -= 1
+        if floor_cordy >= 3500 and player_cordy >= 30 and player_cordy <= height // 2:
+            player_cordy -= speed
+        elif floor_cordy <= 3500 and player_cordy == height // 2:
+            floor_cordy += speed
+        elif player_cordy >= height // 2:
+            player_cordy -= speed
 
         char = pygame.transform.rotate(char, abs(360 - rotation))
         rotation = 0
       
     elif keys[K_LEFT] or keys[K_a]:
-        if floor_cordx >= 4500 and player_cordx >= 30 and player_cordx <= 450:
-            player_cordx -= 1
-        elif floor_cordx <= 4500 and player_cordx == 450:
-            floor_cordx += 1
-        elif player_cordx >= 450:
-            player_cordx -= 1
+        if floor_cordx >= 4500 and player_cordx >= 30 and player_cordx <= width // 2:
+            player_cordx -= speed
+        elif floor_cordx <= 4500 and player_cordx == width // 2:
+            floor_cordx += speed
+        elif player_cordx >= width // 2:
+            player_cordx -= speed
 
         char = pygame.transform.rotate(char, abs(360 - rotation + 90))
         rotation = 90
             
     elif keys[K_RIGHT] or keys[K_d]:
-        if floor_cordx <= -3600 and player_cordx <= 870 and player_cordx >= 450:
-            player_cordx += 1
-        elif floor_cordx >= -3600 and player_cordx == 450:
-            floor_cordx -= 1
-        elif player_cordx <= 450:
-            player_cordx += 1
+        if floor_cordx <= -2580 and player_cordx <= 1890 and player_cordx >= width // 2:
+            player_cordx += speed
+        elif floor_cordx >= -2580 and player_cordx == width // 2:
+            floor_cordx -= speed
+        elif player_cordx <= width // 2:
+            player_cordx += speed
 
         char = pygame.transform.rotate(char, abs(360 - rotation + 270))
         rotation = 270
 
+    """if count == 0:
+        char = pygame.image.load("pictures/1.png")
+    elif count == 1:
+        char = pygame.image.load("pictures/2.png")
+    elif count == 2:
+        char = pygame.image.load("pictures/1.png")
+    elif count == 3:
+        char = pygame.image.load("pictures/3.png")
 
+    if count < 4:
+        count += 1
+    else:
+        count = 0"""
 
     floor_rect.center = floor_cordx, floor_cordy
     player_rect.center = player_cordx, player_cordy
-
     screen.blit(floor, floor_rect)
     screen.blit(char, player_rect)
     pygame.display.update()
