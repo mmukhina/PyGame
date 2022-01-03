3bc1aec0a682c60a356b3198bd4c2cbbb49e0bce
Mayya <mayya.mukhina@icloud.com>
2022-01-03 19:13:10 +0300

    joystick code

---
+++ Button/Button.ino
@@ -0,0 +1,79 @@
+#define LED 8
+
+const int buttonPin1 = 2;     
+const int buttonPin2 = 3;
+const int buttonPin3 = 4;
+const int buttonPin4 = 5;
+
+const int xPin = A1;
+const int yPin = A0;
+int xPos = 0;
+int yPos = 0;
+
+int buttonState1 = 0;         
+int buttonState2 = 0;
+int buttonState3 = 0;
+int buttonState4 = 0;
+
+char character;
+String data = "";
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
+  pinMode(buttonPin3, INPUT);
+  pinMode(buttonPin4, INPUT);
+
+  pinMode(xPin, INPUT);
+  pinMode(yPin, INPUT);
+
+  delay(1000);
+  while (Serial.read() != '1'){
+  }
+  digitalWrite(LED, HIGH);
+}
+
+void loop() {
+  buttonState1 = digitalRead(buttonPin1);
+  buttonState2 = digitalRead(buttonPin2);
+  buttonState3 = digitalRead(buttonPin3);
+  buttonState4 = digitalRead(buttonPin4);
+
+  xPos = analogRead(xPin);
+  yPos = analogRead(yPin);
+ 
+  if (buttonState1 == HIGH) {
+    data = '1';
+  } else if (buttonState2 == HIGH) {
+    data = '2';
+  } else if (buttonState3 == HIGH) {
+    data += '3';
+   }else if (buttonState4 == HIGH) {
+    data = '4';
+  } else{
+    data = '5';
+  }
+
+  if (xPos < 1000 and xPos > 20 and yPos < 20) {
+    data += 'R';
+  } else if (xPos < 1000 and xPos > 20 and yPos > 1000) {
+    data += 'L';
+  } else if (yPos < 1000 and yPos > 20 and xPos < 20){
+    data += 'U';
+  } else if (yPos < 1000 and yPos > 20 and xPos > 1000){
+    data += 'D';
+  } else{
+    data += 'N';
+  }
+    
+  Serial.print(data);
+  data = "";
+}
+
+  
+  
+
+ 
---
+++ bluetooth_file.py
@@ -0,0 +1,48 @@
+import time
+import serial
+import serial.tools.list_ports
+
+def findArduinoUnoPort():
+        portList = list(serial.tools.list_ports.comports())
+        for port in portList:
+            if ("VID:PID=2341:0043" in port[0] or
+                "VID:PID=2341:0043" in port[1] or
+               "VID:PID=2341:0043" in port[2]):
+                    return port[0]
+
+
+def connect_btn():
+    unoPort = findArduinoUnoPort()
+
+    if unoPort:
+        try:
+            arduino = serial.Serial(port='COM3', baudrate=9600,timeout=0.1)
+            print("Success")
+            return arduino
+
+        except Exception:
+            print("Error")
+
+
+
+def write_read(data, arduino):
+        arduino.write(bytes(data, 'utf-8'))
+
+
+
+arduino = connect_btn()
+time.sleep(2)
+if arduino:
+    write_read("1", arduino)
+    
+while True:
+    try:
+        a = arduino.readline()
+        print(a)
+    except Exception:
+        print("Error")
+
+
+
+
+        
